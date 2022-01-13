e8793778ecd64dfae82b8a3b6642a00120a635d6
mihaibiancadaria <mihaibiancadaria@gmail.com>
2022-01-13 17:52:28 +0200

    Authentication

--- cloudprojectseminar/src/main/java/com/example/cloudprojectseminar/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cloudprojectseminar;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id) {
-       super("User with id " + id + " doesn't exist ");
-    }
-}
